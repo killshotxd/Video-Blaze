06e1e6ff316caffa7d08cb77e61a134532de29da
killshotxd <hassanansari211@gmail.com>
2022-09-28 11:10:41 +0530

    ALL APIS saved

---
+++ src/firebase-config.jsx
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAaMtjZV-SAwbk7HZ13hl5W66Pgm_2VUYU",
+  authDomain: "fir-auth-b1867.firebaseapp.com",
+  projectId: "fir-auth-b1867",
+  storageBucket: "fir-auth-b1867.appspot.com",
+  messagingSenderId: "762749686456",
+  appId: "1:762749686456:web:4acc5fc073b9e145ecda4d",
+  measurementId: "G-MX7HJWKR4V",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
