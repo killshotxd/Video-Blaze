a82c6fec783cdfe1615bac6c0e7fd0838c3a9824
killshotxd <hassanansari211@gmail.com>
2022-09-28 10:52:05 +0530

    Chakra ui implement

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeScript } from "@chakra-ui/react";
+import { theme } from "./Theme";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </ChakraProvider>
 );
