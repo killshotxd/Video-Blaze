45962332139806bfac6bfb79e098e66a22b1b8d5
killshotxd <hassanansari211@gmail.com>
2022-09-28 16:21:59 +0530

    BG setup

--- src/Container/Login.jsx
+++ src/Container/Login.jsx
@@ -1,7 +1,21 @@
+import { Flex } from "@chakra-ui/react";
 import React from "react";
-
+import { Image } from "@chakra-ui/react";
+import MusicBg from "../assets/musicbg.jpg";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Flex
+      justifyContent={"center"}
+      alignItems={"center"}
+      width={"100vw"}
+      height={"100vh"}
+      position={"relative"}
+    >
+      <Image src={MusicBg} objectFit="cover" width={"full"} height={"full"} />
+    </Flex>
+  );
 };
 
 export default Login;
+
+// 48:22
