86f7537ef4e529978e6052e731b0548dec65ef71
killshotxd <hassanansari211@gmail.com>
2022-09-29 13:45:21 +0530

    Update Login.jsx

--- src/Container/Login.jsx
+++ src/Container/Login.jsx
@@ -1,7 +1,8 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex, HStack } from "@chakra-ui/react";
 import React from "react";
 import { Image } from "@chakra-ui/react";
 import MusicBg from "../assets/musicbg.jpg";
+import { FcGoogle } from "react-icons/fc";
 const Login = () => {
   return (
     <Flex
@@ -12,6 +13,26 @@ const Login = () => {
       position={"relative"}
     >
       <Image src={MusicBg} objectFit="cover" width={"full"} height={"full"} />
+      <Flex
+        position={"absolute"}
+        width={"100vw"}
+        height={"100vh"}
+        bg={"blackAlpha.600"}
+        top={0}
+        left={0}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <HStack>
+          <Button
+            leftIcon={<FcGoogle fontSize={25} />}
+            colorScheme={"whiteAlpha"}
+            shadow={"lg"}
+          >
+            SignIn with Google
+          </Button>
+        </HStack>
+      </Flex>
     </Flex>
   );
 };
