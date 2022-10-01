fbeb778423f8edafb52f2874f7cbd4e165966290
killshotxd <hassanansari211@gmail.com>
2022-10-01 16:20:49 +0530

    Create PAge

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -1,7 +1,33 @@
+import { Flex, useColorMode, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 
 const Create = () => {
-  return <div>Create</div>;
+  const { colorMode } = useColorMode();
+  const bg = useColorModeValue("gray.50", "gray.900");
+  const textColor = useColorModeValue("gray.900", "gray.50");
+
+  return (
+    <Flex
+      justifyContent={"center"}
+      alignItems="center"
+      width={"full"}
+      minHeight="100vh"
+      padding={10}
+    >
+      <Flex
+        width={"80%"}
+        height="full"
+        border={"1px"}
+        borderColor="gray.300"
+        borderRadius={"md"}
+        p="4"
+        flexDirection={"column"}
+        alignItems="center"
+        justifyContent={"center"}
+        gap={2}
+      ></Flex>
+    </Flex>
+  );
 };
 
 export default Create;
--- src/Container/Home.jsx
+++ src/Container/Home.jsx
@@ -17,24 +17,32 @@ const Home = ({ user }) => {
     <>
       <Navbar user={user} />
 
-      <Flex
-        direction={"column"}
-        justifyContent="start"
-        alignItems={"center"}
-        width="20"
-      >
-        {categories &&
-          categories.map((data) => <Category key={data.id} data={data} />)}
-      </Flex>
+      <Flex width={"100vw"}>
+        <Flex
+          direction={"column"}
+          justifyContent="start"
+          alignItems={"center"}
+          width="5%"
+        >
+          {categories &&
+            categories.map((data) => <Category key={data.id} data={data} />)}
+        </Flex>
 
-      <Flex width={"full"} justifyContent="center" alignItems={"center"} px="4">
-        <Routes>
-          <Route path="/" element={<Feed />} />
-          <Route path="/category/:categoryId" element={<Feed />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/videoDetail/:videoId" element={<VideoPin />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
+        <Flex
+          width={"95%"}
+          px={4}
+          justifyContent="center"
+          alignItems={"center"}
+          px={4}
+        >
+          <Routes>
+            <Route path="/" element={<Feed />} />
+            <Route path="/category/:categoryId" element={<Feed />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/videoDetail/:videoId" element={<VideoPin />} />
+            <Route path="/search" element={<Search />} />
+          </Routes>
+        </Flex>
       </Flex>
     </>
   );
