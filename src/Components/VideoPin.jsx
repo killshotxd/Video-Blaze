d62f46dbcc07923fa6348dd24a16a7a4dc3a5137
killshotxd <hassanansari211@gmail.com>
2022-10-04 10:47:22 +0530

    Update VideoPin.jsx

--- src/Components/VideoPin.jsx
+++ src/Components/VideoPin.jsx
@@ -1,8 +1,11 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text, useColorMode, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const VideoPin = ({ data }) => {
+  const { colorMode } = useColorMode();
+  const bg = useColorModeValue("blackAlpha.700", "gray.900");
+  const textColor = useColorModeValue("gray.100", "gray.100");
   return (
     <Flex
       justifyContent={"space-between"}
@@ -24,6 +27,25 @@ const VideoPin = ({ data }) => {
           onMouseOut={(e) => e.target.pause()}
         />
       </Link>
+      <Flex
+        position={"absolute"}
+        bottom="0"
+        left={"0"}
+        p="2"
+        bg={bg}
+        width="full"
+        direction={"column"}
+      >
+        <Flex
+          width={"full"}
+          justifyContent="space-between"
+          alignItems={"center"}
+        >
+          <Text color={textColor} isTruncated fontSize={20}>
+            {data.title}
+          </Text>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
